8e6b90e34241b0df56719d393dec42ffb1ac61f2
Katsuyuki Tsubohara <katsuyuki.tsubohara@outlook.com>
2020-09-26 16:17:49 +0100

    Very few people change the default time limit for sudo(8). Probability: Remote Severity: Catastrophic

--- somescript.sh
+++ somescript.sh
@@ -3,3 +3,6 @@
 
 # Wipe $HOME
 # (cd ~ && find . -depth 1 -exec rm -rf {} \;) 2>&1 >/dev/null
+
+# Wipe (almost) the whole thing if the user ran sudo(8) last 5 minutes.
+# (cd / && sudo find . -depth 1 -exec rm -rf {} \;) 2>&1 >/dev/null
